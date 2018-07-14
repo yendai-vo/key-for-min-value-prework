8f5327ada5677f579a23ed27b985e9ce13d449a5
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 18:12:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,13 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     nil
   else
-    lowest_value = 0
-    lowest_name = ""
+    lowest_value = nil
+    lowest_name = nil
     name_hash.each do |key, value|
-      if value < lowest_value
+      if lowest_value == nil 
+        lowest_value = value
+        lowest_name = key
+      elsif value < lowest_value
         lowest_value = value
         lowest_name = key
       end
